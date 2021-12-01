3ae8c9b7a7afe80256fce9d5acceb1ae62594725
Abderrahim Zabir <azabir@e2r9p6.1337.ma>
2021-12-01 06:15:21 +0100

    one LEAK no norms

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -9,11 +9,16 @@
 /*   Updated: 2021/11/27 23:03:18 by azabir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "get_next_line.h"
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
 void	ft_check(char *s, int *i)
 {
+	*i = 0;
 	while (s[*i] != '\0')
 	{
 		if (s[*i] == '\n')
@@ -22,52 +27,76 @@ void	ft_check(char *s, int *i)
 	}
 }
 
+char	*ft_read_file(int fd, char *left_str)
+{
+	char	*buff;
+	int		rd_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = read(fd, buff, BUFFER_SIZE);
+	if (rd_bytes == -1)
+	{
+		free(buff);
+		return (NULL);
+	}
+	buff[rd_bytes] = '\0';
+	while (rd_bytes > 0)
+	{
+		left_str = ft_strjoin(left_str, buff);
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[rd_bytes] = '\0';
+	}
+	free(buff);
+	return (left_str);
+}
+
 char	*get_next_line(int fd)
 {
-	static char			*str = "";
-	char				*tmp;
-	ssize_t				len;
-	int					i;
-	char				*save;
+	char		*line;
+	static char	*str;
+	int 		i;
 
-	i = 0;
-	tmp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!tmp)
-		return (0);
-	len = read(fd, tmp, BUFFER_SIZE);
-	while (len > 0)
-	{
-		tmp[len] = '\0';
-		str = ft_strjoin(str, tmp);
-		len = read(fd, tmp, BUFFER_SIZE);
-	}
-	free(tmp);
-	save = ft_strdup(str);
-	if (*save == '\0')
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+	if (!str)
+	{
+		str = malloc(sizeof(char) * 1);
+		str[0] = 0;
+		str = ft_read_file(fd, str);
+		if (!str)
+			return (NULL);
+	}
+	line = ft_strdup(str);
+	if (*line == '\0')
+	{
+		free (line);
+		return (NULL);
+	}
 	ft_check(str, &i);
 	str = str + i + 1;
-	save[i + 1] = '\0';
-	return (save);
+	line[i + 1] = '\0';
+	return (line);
 }
 /*
 int	main(void)
 {
 	int	fd;
 
-	fd = open("text.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
+	fd = open ("test2.txt", O_RDONLY);
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
 }
-*/
+ */
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -19,8 +19,17 @@
 # include <stdio.h>
 # include <unistd.h>
 
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s1);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
 char	*get_next_line(int fd);
+char	*ft_read_to_left_str(int fd, char *left_str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+size_t	ft_strlen(char *s);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
+char	*ft_strdup(char *s1);
 
 #endif

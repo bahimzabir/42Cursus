740ee9052131452bbe1e4b1b7f55692b53e18693
bahimzabir <azabir@student.1337.ma>
2022-03-24 20:24:24 +0100

    pa and pb done

--- push_swap/ft_pb.c
+++ push_swap/ft_pb.c
@@ -1,24 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_pa.c                                            :+:      :+:    :+:   */
+/*   ft_pb.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: azabir <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/24 16:31:52 by azabir            #+#    #+#             */
-/*   Updated: 2022/03/24 16:31:54 by azabir           ###   ########.fr       */
+/*   Created: 2022/03/24 20:22:20 by azabir            #+#    #+#             */
+/*   Updated: 2022/03/24 20:22:22 by azabir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "push_swap.h"
 
 void	ft_pa(t_list **sta, t_list **stb)
 {
 	t_list	*temp;
-	temp = *stb;
-	*stb = (*stb)->next;
-	(*stb)->prev = NULL;
-	temp->next = *sta;
-	(*sta)->prev = temp;
-	*sta = temp;
+	temp = *sta;
+	*sta = (*sta)->next;
+	(*sta)->prev = NULL;
+	temp->next = *stb;
+	(*stb)->prev = temp;
+	*stb = temp;
 }
